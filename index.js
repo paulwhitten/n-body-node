a43b2a76a9987f978af4c7537e810d79b8a3a478
paul.whitten <paul.whitten@ibm.com>
2017-05-11 11:13:43 -0400

    comment out timing code

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var addon = require('./build/Release/nBody.node');
 var process = require('process');
 
-var startTime = process.hrtime();
+// take initial high resolution timestamp
+//var startTime = process.hrtime();
+
+// Would be nice if the module was more generic and we passed the
+// body specifics into the module.
 
 addon.nBody(process.argv[2], (err, result) => {
     console.log(result.toPrecision(9));
 });
 
+// take timestamp on exit, printing results.
+/*
 process.on('exit', ()=> {
     var duration = process.hrtime(startTime);
     console.log(`Benchmark took ${duration[0] * 1e9 + duration[1]} nanoseconds`);
 });
+*/
